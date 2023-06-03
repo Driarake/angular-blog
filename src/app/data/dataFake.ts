f61345e382ef9909dfd5ea5ed931c950aaefccc1
Driarake <adriana_drix@yahoo.com.br>
2023-06-02 23:00:55 -0300

    "content created"

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake= [
+  {
+    "id": "1",
+    "title":"NOVA FORÇA ANGULAR ESTÁ CHEGANDO",
+    "description":"SOON...",
+    "photoCover": "https://th.bing.com/th/id/R.2dbf612c72a9aca9dafe17e6dcd64c37?rik=YVVE5ofLOAAWgg&riu=http%3a%2f%2fwallpapercave.com%2fwp%2fVQEbGjY.jpg&ehk=drY3lAaMxCxe9Mvq9EByBYFgCtiglcr81q3CTeyLwfA%3d&risl=&pid=ImgRaw&r=0"
+  },
+  {
+    "id": "2",
+    "title":"SOON...",
+    "description":"The Power...",
+    "photoCover": "https://th.bing.com/th/id/OIP.xO2Pga2jyJHej4OaJdiz-gAAAA?pid=ImgDet&rs=1"
+  },
+  {
+    "id": "3",
+    "title":"😨...",
+    "description":"",
+    "photoCover": "https://th.bing.com/th?id=OIF.0F5ICl4a1WY8%2f2Ws1%2fUFYA&pid=ImgDet&rs=1"
+  },
+  {
+    "id": "4",
+    "title":"WILL BE THE FINAL BATTLE?...",
+    "description":"",
+    "photoCover": "https://th.bing.com/th?id=OIF.d1%2f67yIzfF17mhJSoxL4QQ&pid=ImgDet&rs=1"
+  }
+]
